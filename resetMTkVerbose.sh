830e51d4f52c6b3dc885707ee222304e2e24c8f3
Leying <leying.hu@innopeaktech.com>
2026-06-09 16:57:52 -0700

    update reset

--- resetMTkVerbose.sh
+++ resetMTkVerbose.sh
@@ -4,23 +4,23 @@ adb remount
  
 adb root
  
-adb shell setprop debug.mtk_tflite.vlog
+adb shell setprop debug.mtk_tflite.vlog false
  
-adb shell setprop debug.nn.vlog
-adb shell setprop debug.neuropilot.vlog
+adb shell setprop debug.nn.vlog 0
+adb shell setprop debug.neuropilot.vlog false
  
 adb root
  
-adb shell setprop persist.vendor.apuware.debug.utilslog
-adb shell setprop persist.vendor.apuware.debug.xrplog
-adb shell setprop persist.vendor.apuware.debug.apusyslog
+adb shell setprop persist.vendor.apuware.debug.utilslog 0
+adb shell setprop persist.vendor.apuware.debug.xrplog 0
+adb shell setprop persist.vendor.apuware.debug.apusyslog 0
  
 adb root
  
 adb shell setprop debug.neuron.adapter.AdapterSetDebugLevel
  
-adb shell setprop debug.neuron.runtime.Verbose
-adb shell setprop debug.neuron.runtime.DumpVerbose
+adb shell setprop debug.neuron.runtime.Verbose false
+adb shell setprop debug.neuron.runtime.DumpVerbose false
  
 adb shell stop neuralnetworks_hal_mtk_neuron_service
 adb shell stop neuralnetworks_hal_service_mtk_neuron
